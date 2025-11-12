f0e251b835f6c3bafdd7a87f2623a91d6f87453a
Gl1f <k.max2517@gmail.com>
2025-11-12 19:06:00 +0300

    Проверка типов данных и практика

--- core/views.py
+++ core/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Name:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def __str__(self):
+        return f"{self.name}, {self.age} лет"
+    
+    def age_in_days(self):
+        return self.age * 365
+
 # Create your views here.
 def index(request):
-    context = {"name": "АРБУЗ"}
+    context = {
+        "name": "Арбуз",
+        "integer": 1,
+        "float": 1.0,
+        "string": "строка",
+        "num_list": [1, 2, 3],
+        "name_dict": {"name": "Арбуз", "price": 100},
+        "string_set": {"red", "green", "yellow"},
+        "name_object": Name("Алевтина", 25),
+    }
     return render(request, "first_template.html", context)
--- sdfiuh.py
+++
@@ -1,8 +0,0 @@
-from math import ceil, log2
-
-answer = 0
-full_memory = int(input()) * 1024
-for i in range(10_000, 0, -1):
-    if (ceil((700 + ceil(log2(i))) / 8) * i) >= full_memory:
-        answer = i - 1
-print(answer)
