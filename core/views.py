192d25c3245146d2295ad71bbda441468544f06e
Gl1f <k.max2517@gmail.com>
2026-01-08 02:23:40 +0300

    проверка разных типов данных в шаблонах

--- core/views.py
+++ core/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from dataclasses import dataclass
 
 
-# Create your views here.
+@dataclass
+class Name:
+    name: str
+    age: int
+
+    @property
+    def age_in_days(self) -> int:
+        return self.age * 365
+
+
 def index(request):
     context = {
-        "name": "Максим",
-        "title": "BarberShop",
+        'title': 'Главная',
+        "name": "Арбуз",
+        "integer": 1,
+        "float": 1.0,
+        "string": "строка",
+        "num_list": [1, 2, 3],
+        "name_dict": {"name": "Арбуз", "price": 100},
+        "string_set": {"red", "green", "yellow"},
+        "name_object": Name("Алевтина", 25),
     }
     return render(request, "core/index.html", context)
