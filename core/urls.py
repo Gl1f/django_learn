da635405460e7958f0a327b6604ba5849ae7c047
Gl1f <k.max2517@gmail.com>
2026-01-08 18:55:10 +0300

    наследование и подключение bootstrap

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("masters/", views.master_list, name="master_list"),
+    path("masters/<int:master_id>/", views.master_detail, name="master_detail"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from dataclasses import dataclass
+from django.http import Http404
 
 
 @dataclass
@@ -11,6 +12,11 @@ class Name:
     def age_in_days(self) -> int:
         return self.age * 365
 
+MASTERS = [
+    {"id": 1, "name": "Андрей", "level": "middle"},
+    {"id": 2, "name": "Олег", "level": "senior"},
+    {"id": 3, "name": "Ирина", "level": "junior"},
+]
 
 def index(request):
     context = {
@@ -25,3 +31,14 @@ def index(request):
         "name_object": Name("Алевтина", 25),
     }
     return render(request, "core/index.html", context)
+
+
+
+def master_list(request):
+    return render(request, "core/master_list.html", {"masters": MASTERS})
+
+def master_detail(request, master_id: int):
+    master = next((m for m in MASTERS if m["id"] == master_id), None)
+    if master is None:
+        raise Http404("Master not found")
+    return render(request, "core/master_detail.html", {"master": master})
