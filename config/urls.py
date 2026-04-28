95c6e246def0e544316b57b3d9943ba7d063b85b
Gl1f <k.max2517@gmail.com>
2026-04-28 13:52:01 +0300

    train_types_templates

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index
+from core.views import index, train
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='home'),
+    path('train/', train, name='train'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Name:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def age_in_days(self):
+        return self.age * 365
+
 # Create your views here.
 def index(request):
     context = {
         'name': "Арбуз",
     }
     return render(request, 'first_template.html', context)
+
+def train(request):
+    context = {
+    "name": "Арбуз",
+    "integer": 1,
+    "float": 1.0,
+    "string": "строка",
+    "num_list": [1, 2, 3],
+    "name_dict": {"name": "Арбуз", "price": 100},
+    "string_set": {"red", "green", "yellow"},
+    "name_object": Name("Алевтина", 25).name,
+    "method_object": Name("Алевтина", 25).age_in_days(),
+    }
+    
+    return render(request, 'train_types.html', context)
